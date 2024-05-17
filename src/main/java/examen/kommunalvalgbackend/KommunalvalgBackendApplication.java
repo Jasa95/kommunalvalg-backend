d918e3a929bfb6bd0f6858e291e5125f2bffde80
Jasa95 <jaco3020@stud.kea.dk>
2024-05-17 10:27:23 +0200

    first commit

---
+++ src/main/java/examen/kommunalvalgbackend/KommunalvalgBackendApplication.java
@@ -0,0 +1,13 @@
+package examen.kommunalvalgbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KommunalvalgBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KommunalvalgBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/examen/kommunalvalgbackend/KommunalvalgBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package examen.kommunalvalgbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KommunalvalgBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
